576c47860311f5162f9e2321d1705cc59dbee26b
vikyatht <38485024+vikyatht@users.noreply.github.com>
2018-04-18 10:06:11 +0530

    Delete connect1.php

--- connect1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host="localhost";
-$dbname="2015cse142";
-$user="root";
-$password="tiger";
-$dbh= mysqli_connect($host,$user,$password,$dbname) or die("Error connecting to database".mysqli_connect($dbh));
-?>
